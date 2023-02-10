0d685bbab60bbf07e9a365e7490433cbd3d12aa3
Dima Livshits <dimonka83@gmail.com>
2023-02-10 14:27:19 +0300

    fix: no license

--- packages/dev-license/src/main/ts/index.ts
+++ packages/dev-license/src/main/ts/index.ts
@@ -1,6 +1,7 @@
 import { Executor } from '@packasso/core'
 
-export const executor: Executor = async ({ copyText }) => {
+export const executor: Executor = async ({ copyJson, copyText }) => {
+  copyJson('package.json')
   copyText('LICENSE', {
     year: new Date().getFullYear(),
   })
