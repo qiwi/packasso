1c99106e6f555a6d1f252a631457aa85452e8d7e
Dima Livshits <dimonka83@gmail.com>
2023-02-21 16:10:54 +0300

    fix: stranger things

---
+++ packages/preset-ts-tsc-jest/src/main/ts/modules.ts
@@ -0,0 +1,9 @@
+export const modules = [
+  '@packasso/dev-license',
+  '@packasso/dev-ts',
+  '@packasso/build-ts-tsc',
+  '@packasso/build-resources',
+  '@packasso/style-eslint-prettier',
+  '@packasso/test-jest',
+  '@packasso/release-zx-semrel',
+]
