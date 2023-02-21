0085e94ed48ba6abc665f69e6336d08a58ffe15f
Dima Livshits <dimonka83@gmail.com>
2023-02-21 14:01:01 +0300

    feat: build

---
+++ packages/build-ts-tsc/src/main/ts/build.ts
@@ -0,0 +1,12 @@
+import { BuildModule } from '@packasso/core'
+
+export const build: BuildModule = async () => ({
+  commands: [
+    ['rimraf target/cjs target/esm'],
+    ['! tsc -b tsconfig.cjs.json tsconfig.esm.json'],
+    [
+      'tsc-esm-fix --target target/cjs --ext .cjs --fillBlank',
+      'tsc-esm-fix --target target/esm --ext .mjs --fillBlank --forceDefaultExport',
+    ],
+  ],
+})
