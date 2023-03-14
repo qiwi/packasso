ca244f6f9ea35a5629f1eb6a43937b3db03de11b
Dima Livshits <dimonka83@gmail.com>
2023-03-14 12:40:30 +0300

    fix: singles

--- packages/test-uvu/src/main/ts/test.ts
+++ packages/test-uvu/src/main/ts/test.ts
@@ -5,8 +5,15 @@ import minimist from 'minimist'
 
 export const test: ModuleCommand = async (pkg, pkgs) => {
   const paths = pkgs.map(({ relPath }) => relPath)
-  const mainPaths = pkg.type === PackageType.TREE ? `{${paths.join(',')}}/` : ''
-  const testPaths = pkg.type === PackageType.TREE ? `(${paths.join('|')})/` : ''
+  const many = paths.length > 1
+  const mainPaths =
+    pkg.type === PackageType.TREE
+      ? `${many ? '{' : ''}${paths.join(',')}${many ? '}' : ''}/`
+      : ''
+  const testPaths =
+    pkg.type === PackageType.TREE
+      ? `${many ? '(' : ''}${paths.join('|')}${many ? ')' : ''}/`
+      : ''
   const u = minimist(argv).u ? 'UPDATE_SNAPSHOTS=true' : ''
   return [
     `! ${u} c8 --all -o target/coverage -r html -r text -r lcov -n '${mainPaths}src/main/{ts,js}' uvu -r tsm -r earljs/uvu . '${testPaths}src/test/[jt]s/.*.(spec|test).[jt]sx?'`,
