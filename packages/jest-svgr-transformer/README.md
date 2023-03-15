5d619500c0c0b09d3d9c875b9e26d0fa0d3e690a
Dima Livshits <dimonka83@gmail.com>
2023-03-15 18:54:23 +0300

    feat: jest svgr transformer (#27)

---
+++ packages/jest-svgr-transformer/README.md
@@ -0,0 +1,18 @@
+# @packasso/jest-svgr-transformer
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/aaced5b2261f8a59b7cd/maintainability)](https://codeclimate.com/github/qiwi/packasso/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/aaced5b2261f8a59b7cd/test_coverage)](https://codeclimate.com/github/qiwi/packasso/test_coverage)
+
+## Usage
+
+```json
+{
+  "transform": {
+    "^.+\\.svg$": "@packasso/jest-svgr-transformer"
+  }
+}
+```
+
+## License
+
+[MIT](./LICENSE)
---
+++ packages/jest-svgr-transformer/src/main/ts/index.ts
@@ -0,0 +1,28 @@
+import { basename, parse } from 'node:path'
+
+import { camelCase, startCase } from 'lodash-es'
+
+export default {
+  process: (source: string, filename: string) => {
+    const content = JSON.stringify(basename(filename))
+    const name = `Svg${startCase(camelCase(parse(filename).name))}`
+    return `
+      const React = require('react');
+      module.exports = {
+        __esModule: true,
+        default: ${content},
+        ReactComponent: React.forwardRef(function ${name}(props, ref) {
+          return {
+            $$typeof: Symbol.for('react.element'),
+            type: 'svg',
+            ref: ref,
+            key: null,
+            props: Object.assign({}, props, {
+              children: ${content}
+            })
+          };
+        }),
+      };
+    `
+  },
+}
