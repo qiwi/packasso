490b0b228ecec6571780685ee3647cd87530a50b
Dima Livshits <dimonka83@gmail.com>
2023-03-16 17:18:37 +0300

    fix: separate preset packasso

--- packages/preset-packasso/src/main/ts/modules.ts
+++ packages/preset-packasso/src/main/ts/modules.ts
@@ -1,1 +1,12 @@
-export const modules = ['@packasso/preset-ts-tsc-uvu', '@packasso/dev-packasso']
+export const modules = [
+  '@packasso/dev-license',
+  '@packasso/dev-ts',
+  '@packasso/dev-packasso',
+  '@packasso/build-ts-tsc',
+  '@packasso/build-resources',
+  '@packasso/buildstamp',
+  '@packasso/style-eslint',
+  '@packasso/style-prettier',
+  '@packasso/test-uvu',
+  '@packasso/release-zx-semrel',
+]
