623267e2398fb56e69c1c7b955411fd4a1b6233d
Dima Livshits <dimonka83@gmail.com>
2023-03-21 11:55:28 +0300

    fix: tsx

--- packages/cli/src/main/ts/index.ts
+++ packages/cli/src/main/ts/index.ts
@@ -1,42 +1,4 @@
 #!/usr/bin/env node
-import { error, log } from 'node:console'
-import { argv, cwd, execArgv, exit } from 'node:process'
+import { execute } from '@packasso/core'
 
-import { command, getRootDir, install, purge } from '@packasso/core'
-import minimist from 'minimist'
-
-const execute: (name: string) => Promise<unknown> = async (name) => {
-  log(`trying to ${name}...`)
-  const root = getRootDir(cwd())
-  const { conditions } = minimist(execArgv)
-  const development = [conditions].flat().includes('development')
-  switch (name) {
-    case 'index':
-    case 'modules':
-      throw new Error(`i can't... you can't... no one can ${name}!`)
-    case 'uninstall':
-      await install(root, development, true)
-      break
-    case 'install':
-      await install(root, development, true)
-      await install(root, development)
-      break
-    case 'purge':
-      await purge(root)
-      break
-    default:
-      await command(name, cwd())
-      break
-  }
-  log(`seems to be a success!`)
-}
-
-execute(argv[2])
-  .then(() => {
-    exit(0)
-  })
-  // eslint-disable-next-line unicorn/prefer-top-level-await
-  .catch((e) => {
-    error(e.message)
-    exit(1)
-  })
+execute()
