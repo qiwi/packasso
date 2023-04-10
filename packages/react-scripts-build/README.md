74c8eff673104712bfc9e2586ac28eee75b451ec
Dima Livshits <dimonka83@gmail.com>
2023-04-10 17:10:07 +0300

    feat: react-scripts build (#33)

---
+++ packages/react-scripts-build/README.md
@@ -0,0 +1,14 @@
+# @packasso/react-scripts-build
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/aaced5b2261f8a59b7cd/maintainability)](https://codeclimate.com/github/qiwi/packasso/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/aaced5b2261f8a59b7cd/test_coverage)](https://codeclimate.com/github/qiwi/packasso/test_coverage)
+
+[CRA Build](https://create-react-app.dev/docs/available-scripts#npm-run-build) compatible with maven standard directory layout
+
+## Usage
+
+[packasso](https://www.npmjs.com/package/packasso)
+
+## License
+
+[MIT](./LICENSE)
---
+++ packages/react-scripts-build/src/main/ts/commands.ts
@@ -0,0 +1,36 @@
+import {
+  cmd,
+  Commands,
+  ContextInstallData,
+  execute,
+  install,
+  uninstall,
+} from '@packasso/core'
+
+const data: ContextInstallData = ({ pkg }) => [
+  pkg.leaf || pkg.unit
+    ? {
+        'package.json': {
+          publishConfig: {
+            files: ['target/webapp'],
+          },
+        },
+        '.env': ['BUILD_PATH=target/webapp', ''].join('\n'),
+      }
+    : {},
+]
+
+export const commands: Commands = {
+  install: async (context) => {
+    await install(data(context), context.pkg)
+  },
+  uninstall: async (context) => {
+    await uninstall(data(context), context.pkg)
+  },
+  clean: async ({ pkg, pkgs }) => {
+    await execute('rimraf target/webapp', pkg.tree ? pkgs : pkg)
+  },
+  build: async ({ pkg, pkgs }) => {
+    await execute(cmd('react-scripts build'), pkg.tree ? pkgs : pkg)
+  },
+}
