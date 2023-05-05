fc218754e264fad89ef430228cfa35b51e7a2b08
Dima Livshits <dimonka83@gmail.com>
2023-05-05 14:07:33 +0300

    fix: handle audit non-zero codes

--- packages/yarn-audit/src/main/ts/commands.ts
+++ packages/yarn-audit/src/main/ts/commands.ts
@@ -1,16 +1,38 @@
 import { cmd, Commands, execute } from '@packasso/core'
+import lodash from 'lodash'
+
+const levelExitCodes: Record<string, number> = {
+  info: 1,
+  low: 2,
+  moderate: 4,
+  high: 8,
+  critical: 16,
+}
 
 export const commands: Commands = {
   audit: async ({ pkg, args }) => {
-    await execute(
-      args.fix
-        ? cmd('yarn-audit-fix', { 'audit-level': 'moderate' })
-        : cmd('yarn npm audit', {
-            recursive: true,
-            all: true,
-            severity: 'moderate',
-          }),
-      pkg,
-    )
+    const { fix = false, level = 'moderate' } = args
+    try {
+      await execute(
+        fix
+          ? cmd('yarn-audit-fix', {
+              'audit-level': level,
+            })
+          : cmd('yarn npm audit', {
+              recursive: true,
+              all: true,
+              severity: level,
+            }),
+        pkg,
+      )
+    } catch (e) {
+      if (
+        lodash.isArray(e) &&
+        e.every((e) => e.exitCode >= levelExitCodes[level])
+      ) {
+        return
+      }
+      throw e
+    }
   },
 }
