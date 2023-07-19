6d0428cadcf95c17a01f2c2a25de1eaee2e1db1f
Dima Livshits <dimonka83@gmail.com>
2023-07-19 11:21:52 +0300

    fix: browserslist (#44)

--- packages/browserslist/src/main/ts/bin.ts
+++ packages/browserslist/src/main/ts/bin.ts
@@ -7,21 +7,23 @@ import {
 } from '@packasso/core'
 
 const install: Install = {
-  data: [
-    {
-      '.browserslistrc': [
-        '[production]',
-        '> 0.2%',
-        'not dead',
-        'not op_mini all',
-        '',
-        '[development]',
-        'last 1 chrome version',
-        'last 1 firefox version',
-        'last 1 safari version',
-        '',
-      ].join('\n'),
-    },
+  data: (pkg) => [
+    pkg.leaf || pkg.unit
+      ? {
+          '.browserslistrc': [
+            '[production]',
+            '> 0.2%',
+            'not dead',
+            'not op_mini all',
+            '',
+            '[development]',
+            'last 1 chrome version',
+            'last 1 firefox version',
+            'last 1 safari version',
+            '',
+          ].join('\n'),
+        }
+      : {},
   ],
 }
 
