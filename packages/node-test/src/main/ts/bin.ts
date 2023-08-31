17c1a4c3cc63bf49dc9b7e519825463baa0f1d09
Choro Abdymanapov <c.abdymanapov@qiwi.com>
2023-08-31 13:06:03 +0600

    fix: add js & *sx support

--- packages/node-test/src/main/ts/bin.ts
+++ packages/node-test/src/main/ts/bin.ts
@@ -14,7 +14,7 @@ const createCommandTest = (name: string, description: string, suffix?: string) =
     .action(async (options) => {
       const { cwd, preset } = options
       const { root } = await getTopo({ cwd }, preset)
-      const files = await glob(`**/**/*.${suffix}.ts`, { ignore: ['node_modules']})
+      const files = await glob(`**/src/test/{ts,js}/**/*.${suffix}.{ts,js,tsx,jsx}`, { ignore: ['node_modules']})
 
       await execute(
         cmd(
