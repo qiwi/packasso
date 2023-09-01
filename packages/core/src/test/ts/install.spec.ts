b474e029026f1c437c60fa7474551da8bb14860b
Dima Livshits <dimonka83@gmail.com>
2023-09-01 16:09:47 +0300

    feat: use node-test

--- packages/core/src/test/ts/install.spec.ts
+++ packages/core/src/test/ts/install.spec.ts
@@ -1,5 +1,5 @@
-import { expect } from 'earljs'
-import { test } from 'uvu'
+import { deepStrictEqual } from 'node:assert'
+import { describe, it } from 'node:test'
 
 import { diffJson } from '../../main/ts/install'
 
@@ -23,53 +23,57 @@ const json = {
 
 const merge = (json: object) => JSON.parse(JSON.stringify(json))
 
-test('diff json', () => {
-  expect(diffJson(json, { a: 'a' })).toEqual(
-    merge({
+describe('install', () => {
+  it('diff json', () => {
+    deepStrictEqual(
+      diffJson(json, { a: 'a' }),
+      merge({
+        ...json,
+        a: undefined,
+      }),
+    )
+    deepStrictEqual(diffJson(json, { n: 'n' }), json)
+    deepStrictEqual(
+      diffJson(json, { f: { a: 'a', b: 'b', c: 'c', d: 'd' } }),
+      merge({ ...json, f: undefined }),
+    )
+    deepStrictEqual(
+      diffJson(json, { d: ['a'] }),
+      merge({
+        ...json,
+        d: ['b', 'c'],
+      }),
+    )
+    deepStrictEqual(
+      diffJson(json, { d: ['a', 'b', 'c', 'n'] }),
+      merge({
+        ...json,
+        d: undefined,
+      }),
+    )
+    deepStrictEqual(diffJson(json, { d: ['n'] }), json)
+    deepStrictEqual(diffJson(json, { e: { a: 'a' } }), {
       ...json,
-      a: undefined,
-    }),
-  )
-  expect(diffJson(json, { n: 'n' })).toEqual(json)
-  expect(diffJson(json, { f: { a: 'a', b: 'b', c: 'c', d: 'd' } })).toEqual(
-    merge({ ...json, f: undefined }),
-  )
-  expect(diffJson(json, { d: ['a'] })).toEqual(
-    merge({
+      e: merge({
+        ...json.e,
+        a: undefined,
+      }),
+    })
+    deepStrictEqual(diffJson(json, { e: { n: 'n' } }), json)
+    deepStrictEqual(diffJson(json, { e: { d: ['a'] } }), {
       ...json,
-      d: ['b', 'c'],
-    }),
-  )
-  expect(diffJson(json, { d: ['a', 'b', 'c', 'n'] })).toEqual(
-    merge({
+      e: merge({
+        ...json.e,
+        d: ['b', 'c'],
+      }),
+    })
+    deepStrictEqual(diffJson(json, { e: { d: ['a', 'b', 'c', 'n'] } }), {
       ...json,
-      d: undefined,
-    }),
-  )
-  expect(diffJson(json, { d: ['n'] })).toEqual(json)
-  expect(diffJson(json, { e: { a: 'a' } })).toEqual({
-    ...json,
-    e: merge({
-      ...json.e,
-      a: undefined,
-    }),
+      e: merge({
+        ...json.e,
+        d: undefined,
+      }),
+    })
+    deepStrictEqual(diffJson(json, { e: { n: ['n'] } }), json)
   })
-  expect(diffJson(json, { e: { n: 'n' } })).toEqual(json)
-  expect(diffJson(json, { e: { d: ['a'] } })).toEqual({
-    ...json,
-    e: merge({
-      ...json.e,
-      d: ['b', 'c'],
-    }),
-  })
-  expect(diffJson(json, { e: { d: ['a', 'b', 'c', 'n'] } })).toEqual({
-    ...json,
-    e: merge({
-      ...json.e,
-      d: undefined,
-    }),
-  })
-  expect(diffJson(json, { e: { n: ['n'] } })).toEqual(json)
 })
-
-test.run()
