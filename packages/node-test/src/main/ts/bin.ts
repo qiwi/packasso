6f43649eb247743a4a6d9202c08ac2f2fedabc55
Dima Livshits <dimonka83@gmail.com>
2023-09-01 16:06:15 +0300

    fix: node-test purge and clean

--- packages/node-test/src/main/ts/bin.ts
+++ packages/node-test/src/main/ts/bin.ts
@@ -2,6 +2,8 @@
 import {
   cmd,
   createCommand,
+  createCommandClean,
+  createCommandPurge,
   execute,
   getTopo,
   program,
@@ -9,46 +11,40 @@ import {
 import fg from 'fast-glob'
 
 const createCommandTest = (name: string, description: string, suffix: string) =>
-  createCommand(name, description)
-    .action(async (options) => {
-      const { cwd, preset } = options
-      const { root, queuePackages } = await getTopo({ cwd }, preset)
-      const files = await fg.glob(
-        root.tree
-          ? queuePackages.map(it => it.relPath + `/src/test/{ts,js}/**/*.${suffix}.{ts,js,tsx,jsx}`)
-          : `src/test/{ts,js}/**/*.${suffix}.{ts,js,tsx,jsx}`,
-        { ignore: ['node_modules'] }
-      )
-      const paths = queuePackages.map(({ relPath }) => relPath)
-      const many = paths.length > 1
-      const mainPaths = `${
-        root.tree
-          ? `${many ? '{' : ''}${paths.join(',')}${many ? '}' : ''}/`
-          : ''
-      }src/main/{ts,js}`
-
-      await execute(
-        cmd(
-          'c8',
-          {
-            r: ['html', 'text', 'lcov'],
-            n: `'${mainPaths}'`,
-            o: './target/coverage',
-            _: cmd(
-              'node',
-              {
-                test: true,
-                loader: 'tsx',
-                _: files
-              }
-            )
-          }
-        ),
-        root
-      )
-    })
+  createCommand(name, description).action(async (options) => {
+    const { cwd, preset } = options
+    const { root, queuePackages } = await getTopo({ cwd }, preset)
+    const srcTest = `src/test/{ts,js}/**/*.${suffix}.{ts,js,tsx,jsx}`
+    const files = await fg.glob(
+      root.tree
+        ? queuePackages.map(({ relPath }) => `${relPath}/${srcTest}`)
+        : srcTest,
+      { ignore: ['node_modules'] },
+    )
+    const paths = queuePackages.map(({ relPath }) => relPath)
+    const many = paths.length > 1
+    const mainPaths = `${
+      root.tree ? `${many ? '{' : ''}${paths.join(',')}${many ? '}' : ''}/` : ''
+    }src/main/{ts,js}`
+    await execute(
+      cmd('c8', {
+        r: ['html', 'text', 'lcov'],
+        n: `'${mainPaths}'`,
+        o: './target/coverage',
+        _: cmd('node', {
+          test: true,
+          loader: 'tsx',
+          _: files,
+        }),
+      }),
+      root,
+    )
+  })
 
 program(
+  createCommandClean(['target/coverage']),
+  createCommandPurge(['coverage', 'jest.config.*', 'tsconfig.test.json']),
+  createCommandTest('test', 'unit tests', '{spec,test,it,e2e}'),
   createCommandTest('test:unit', 'unit tests', '{spec,test}'),
   createCommandTest('test:it', 'integration tests', 'it'),
   createCommandTest('test:e2e', 'end-to-end tests', 'e2e'),
